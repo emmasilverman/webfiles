5291e90e57683a56526191e682d538f0cfbe90a8
Emma Silverman <emsi6410@colorado.edu>
2021-12-09 16:54:59 -0700

    go

---
+++ portfolio/index.js
@@ -0,0 +1,39 @@
+  // var vg1 = document.getElementById("vg1");
+  // vg1.play()
+  // vg1.volume = 0.5;
+  //
+  // vg1.addEventListener('click', function (){
+  //   if (vg1.paused == false){
+  //     vg1.pause();
+  //     vg1.firstChild.nodeValue="Play";
+  //   } else {
+  //     vg1.play();
+  //     vg1.firstChild.nodeValue = "Pause";
+  //   }
+  // });
+//   var vid = document.getElementById("vg2_2");
+//
+//   function playVid(){
+//     vid.play();
+//   }
+//
+//   function pauseVid(){
+//     vid.pause();
+//   }
+//
+// var vid = document.getElementById("vg1_1");
+// function playVid(){
+//   vid.play();
+// }
+// function pauseVid(){
+//   vid.pause();
+// }
+var vid = document.getElementById("myVideo");
+
+function playVid() {
+  vid.play();
+}
+
+function pauseVid() {
+  vid.pause();
+} 

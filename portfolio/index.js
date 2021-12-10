4e7921aa9c83ec071be6606526756e328c7f3589
Emma Silverman <emsi6410@colorado.edu>
2021-12-09 21:22:30 -0700

    help

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,31 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
   // var vg1 = document.getElementById("vg1");
   // vg1.play()
   // vg1.volume = 0.5;
@@ -28,12 +56,17 @@
 // function pauseVid(){
 //   vid.pause();
 // }
-var vid = document.getElementById("myVideo");
+// var vid = document.getElementById("myVideo1");
+//
+// function playVid() {
+//   vid.play();
+// }
+//
+// function pauseVid() {
+//   vid.pause();
+// }
 
-function playVid() {
-  vid.play();
-}
-
-function pauseVid() {
-  vid.pause();
-} 
+// function loopVideo() {
+// 	 // allows video pulled to a stack
+// 	 video.getVideo().play(pointerToVideo, 'Video', -->Image.videoPdb);
+// }
